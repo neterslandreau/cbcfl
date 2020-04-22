fdc212abed616add1c9c1ee88c2da52dbf6c9fb0
Niles Rowland <neterslandreau@gmail.com>
2020-04-22 17:27:25 -0400

    update foundation.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->primary('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('nickname');
+            $table->string('nickname')->nullable();
             $table->string('email')->unique();
             $table->string('role');
             $table->string('password');
