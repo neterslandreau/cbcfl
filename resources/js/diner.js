1c16bd678f6302c1f21ba2a6c45577b64528fc58
Niles Rowland <neterslandreau@gmail.com>
2020-04-29 16:17:18 -0400

    fix map loading on all pages.

--- resources/js/diner.js
+++ resources/js/diner.js
@@ -1,5 +1,7 @@
 $(function() {
-    Gmap.populateMap('4650 SW 30th Ave, Fort Lauderdale, FL 33312', 'Community Bible Church');
+    if (!window.location.pathname.match(/home|articles/)) {
+        Gmap.populateMap('4650 SW 30th Ave, Fort Lauderdale, FL 33312', 'Community Bible Church');
+    }
 
     $('.nav-link').on('click', function() {
         $('#navbarSupportedContent').removeClass('show');
