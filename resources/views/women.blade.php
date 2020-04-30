c90cf853b3585a6ef79b2bb82f1f4a10af825ef2
Niles Rowland <neterslandreau@gmail.com>
2020-04-30 18:26:10 -0400

    do womens ministry.

--- resources/views/women.blade.php
+++ resources/views/women.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
     <div class="continer pt-5 mt-5">
-        Women's Ministry
+
+        <div class="col-12" style="text-align:center;color:darkred">
+
+            <div class="icon"><i class="fa fa-female fa-5x"></i></div>
+            <h4 class="title">Womens Ministry</h4>
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+
+                <img class="col-12" src="/img/womens-ministry.jpg">
+
+            </div>
+
+            <div class="col-sm">
+                <p class="description">
+                    The ladies of Community Bible Church Dania Beach meet on the first Thursday of each month for  a time of fellowship and spiritual growth at 6:30pm in the Fellowship Hall. All are welcome to attend.                </p>
+
+            </div>
+        </div>
+
     </div>
 @endsection
