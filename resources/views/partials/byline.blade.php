528e51e71b86acf3691b1e5ebd9d587ad2b82b49
Niles Rowland <neterslandreau@gmail.com>
2020-05-12 16:18:43 -0400

    minor fix.

--- resources/views/partials/byline.blade.php
+++ resources/views/partials/byline.blade.php
@@ -1,3 +1,8 @@
+@if(!strlen($article->user->nickname))
+    @php
+        $article->user->nickname = $article->user->first_name . ' ' . $article->user->last_name;
+    @endphp
+@endif
 <p class="blog-post-meta">
 
     <a href="#">{{ $article->user->nickname }}</a> published
