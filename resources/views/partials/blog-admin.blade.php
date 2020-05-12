cecced688054edf4f5cdba76471a45205f588b77
Niles Rowland <neterslandreau@gmail.com>
2020-05-11 20:59:57 -0400

    change things.

--- resources/views/partials/blog-admin.blade.php
+++ resources/views/partials/blog-admin.blade.php
@@ -1,19 +1,19 @@
 @if(isset(auth()->user()->role) && auth()->user()->role == 'staff')
 
     <div class="row">
-        <div class="col-sm-1">
+        <div class="pl-2 pr-2">
             <a href="/articles/create">
                 <button class="btn btn-success btn-sm">New</button>
             </a>
         </div>
         @if (request()->path() !== 'articles')
             @if (($owner ?? ''))
-            <div class="col-sm-1">
+            <div class="pr-2">
                 <a href="/articles/{{ $article->slug }}/edit">
                     <button class="btn btn-warning btn-sm">Edit</button>
                 </a>
             </div>
-            <div class="col-sm-1">
+            <div class="pr-2">
                 <a href="/articles/{{ $article->slug }}/delete">
                     <button class="btn btn-danger btn-sm">Delete</button>
                 </a>
